b3ea6af9464875e535187f0d5e36c81b640404c2
Vít Ondruch <vondruch@redhat.com>
2023-07-19 12:59:08 +0200

    Rescure Errno::ESRCH

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -325,7 +325,11 @@ end
 def cleanup_process(pid)
   puts "* cleanup_process: #{pid}"
   p Process.kill(0, pid)
-  Process.kill(9, pid)
+  begin
+    Process.kill(9, pid)
+  rescue SystemCallError => e
+    raise e unless e.errno == Errno::ESRCH
+  end
   unless RUBY_PLATFORM == 'java'
     Process.wait(pid, Process::WNOHANG)
   end
